4962b94ed75dcf4145bc069a52ef549327e6368c
NaderTate <nadertate@gmail.com>
2023-12-04 11:43:11 +0200

    Added revalidate to schedule page

--- app/(protected)/dashboard/schedule/page.tsx
+++ app/(protected)/dashboard/schedule/page.tsx
@@ -1,5 +1,6 @@
 import Schedule from "./Schedule";
 import prisma from "@/lib/prisma";
+export const revalidate = 100;
 async function page() {
   const schedule = await prisma.schedule.findFirst({
     select: {
